6e5ca6ade6a746521104e4e8d63952bceeddaaf7
Dev gandhi <devgandhi2705@gmail.com>
2024-09-01 03:43:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 df = pd.read_csv("housing.csv")
-print(df)
-#profile = ProfileReport(df)
-#profile.to_file(output_file="housing.html")
+profile = ProfileReport(df)
+profile.to_file(output_file="housing.html")
